e391d17c94ee224bb842a2b8f5369687d6950a34
wangdongcheng <tony.wangdongcheng@qq.com>
2020-03-19 22:20:13 +0800

    tmp

--- src/0106_construct-binary-tree-from-inorder-and-postorder-traversal.js
+++ src/0106_construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -30,6 +30,28 @@ const tree = require("../module/binary_tree");
  * @param {number[]} postorder
  * @return {TreeNode}
  */
-var buildTree = function(inorder, postorder) {
+var buildTree = function (inorder, postorder) {
+    if (inorder.length === 0 && postorder.length === 0) return null;
 
+    let root = new tree.TreeNode(postorder[postorder.length - 1]),
+        rootIdx = inorder.indexOf(root.val),
+        leftInOrder = [],
+        rightInOrder = [],
+        leftPostOrder = [],
+        rightPostOrder = [];
+
+    for (let i = 0; i < rootIdx; i++) {
+        leftInOrder.push(inorder[i]);
+        leftPostOrder.push(postorder[postorder.indexOf(inorder[i])]);
+    }
+    root.left = buildTree(leftInOrder, leftPostOrder);
+    for (let i = rootIdx + 1; i < inorder.length; i++) {
+        rightInOrder.push(inorder[i]);
+        rightPostOrder.push(postorder[postorder.indexOf(inorder[i])]);
+    }
+    root.right = buildTree(rightInOrder, rightPostOrder);
+
+    return root;
 };
+
+console.log(buildTree([9,3,15,20,7],[9,15,7,20,3]), tree.getTreeFromLayerOrderArray([3,9,20,null,null,15,7]));
