4fb0289536a2afaeb8ea4656ec10931c5011fcc5
wangdongcheng <tony.wangdongcheng@qq.com>
2020-04-07 23:51:28 +0800

    20200407

--- _done/1402_reducing-dishes.js
+++ _done/1402_reducing-dishes.js
@@ -38,45 +38,61 @@
  */
 var maxSatisfaction = function (satisfaction) {
     if (satisfaction.findIndex(val => {
-        return val >= 0
+        return val >= 0;
     }) == -1) {
         return 0;
     }
+    let sum = 0;
     if (satisfaction.findIndex(val => {
         return val < 0
     }) == -1) {
-        let sum = 0;
-        satisfaction.sort();
+        satisfaction.sort((a, b) => a - b);
         satisfaction.forEach((val, idx) => {
             sum += val * (idx + 1);
         })
         return sum;
     }
-    let pstv = 0,
-        arr = [],
+
+    let pstvSum = 0,
+        roundSum = 0,
+        pos = [],
         neg = [];
 
-    satisfaction.forEach(val => {
+    satisfaction.forEach((val, idx) => {
         if (val >= 0) {
-            pstv += val;
-            arr.push(val);
-        }
-    })
-
-    satisfaction.forEach(val => {
-        if (val < 0 && Math.abs(val) < pstv) {
+            roundSum += val;
+            pos.push(val);
+        } else if (val < 0) {
             neg.push(val);
         }
     })
-    neg.sort((a, b) => b - a);
-    neg = neg.concat(arr);
-    let sum = 0;
-    neg.forEach((val, idx) => {
-        sum += val * (idx + 1);
+    pos.sort((a, b) => a - b);
+    pos.forEach((val, idx) => {
+        pstvSum += val * (idx + 1);
     })
+    sum = pstvSum;
+    neg.sort((a, b) => b - a);
+    let negRoundSum = 0,
+        negSum = 0;
+
+    for (let i = 0; i < neg.length; i++) {
+        negRoundSum = 0;
+        for (let j = 0; j <= i; j++) {
+            negRoundSum += neg[j];
+        }
+        negSum += negRoundSum;
+        sum = Math.max(sum, pstvSum + (i + 1) * roundSum + negSum);
+    }
+
     return sum;
 };
 
 console.log(maxSatisfaction([-1, -8, 0, 5, -9]), 14);
+console.log(maxSatisfaction([-3, -2, -1, 0, 1, 2, 3]), 28);
 console.log(maxSatisfaction([-1, -4, -5]), 0);
 console.log(maxSatisfaction([4, 3, 2]), 20);
+console.log(maxSatisfaction([-2, 5, -1, 0, 3, -3]), 35);
+console.log(maxSatisfaction([0, -1, 2, -3, 4, 5, -25, 6, 9, 33]), 494);
+console.log(maxSatisfaction([-1, 2, -3, 4, 5, -25, 6, 9, 33]), 435);
+console.log(maxSatisfaction([-188, -61, 719, 123, -920, -801, -24, -764, 703, 564, 704, 573, -673, -691, 328, -858, 977, 980, -119, 739, -823, -527, -544, 332, 191, 33, 919, -671, 165, -303, -235, -718, -913, -403, -67, -297, -977, -90, -61, 128, -982, -997, -463, -104, -29, -195, -133, -904, 160, -109, -992, -210, -360, -367, 49, 753, -292, 23, 917, -522, -563, -171, 83, -29, -730, 518, -917, 84, -72, -170, 807, 583, 894, -770, 646, -570, 373, 609, -29, 500, -536, 106, -800, 501, 901, 132, -873, 237]), 954209);
+console.log(maxSatisfaction([-188, -61, 719, 123, -920, -801, -24, -764, 703, 564, 704, 573, -673, -691, 328, -858, 977, 980, -119]), 72236);
